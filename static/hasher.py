5ed596b0239c2f55b2b553174ef6457cad7a64e4
duenohfao <ngedric74@gmail.com>
2025-01-15 09:36:46 +0800

    finished hasher function

--- static/hasher.py
+++ static/hasher.py
@@ -3,4 +3,6 @@ def to_hash(input_str: str)-> str:
     '''Returns the has value of a string'''
     
     hasher = hashlib.new('SHA256')
+    input_str = bytes(input_str)
     hasher.update(input_str)
+    return hasher.hexdigest()
